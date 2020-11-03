19c350a314d230753de07d0bf5c1b84cbbf9eb51
Ourai Lin <ourairyu@gmail.com>
2020-11-03 13:22:05 +0800

    feat: install Baidu statistic code

---
+++ _assets/javascripts/statistic.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?5747b1551cf437373ae0518cb63a8cdd";
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(hm, s);
+})();
